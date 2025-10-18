43c52829fd14a4dbe194d6cf84750878a0ff892e
bmucf <br407069@ucf.edu>
2025-10-18 14:49:29 -0400

    added fish base class

---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class Fish : MonoBehaviour
+{
+    public string fishName;
+    public int points;
+    public int speed;
+    public int size;
+}
